f025fd4523631884602039bf9ba10f62d73ca3fa
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:08:04 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LENSKO';
+export const nameSongs = [
+	"LET'S GO!", /*1*/
+	"CIRCLES", /*2*/
+	"CETUS", /*3*/
+	"REBIRTH", /*4*/
+	"SARVAGON", /*5*/
+	"TITSEPOKEN", /*6*/
+	"MIGRAINE", /*7*/
+	"LUMINOUS", /*8*/
+	"ANDROMEDA", /*9*/
+	"BOURBON STREET 2015", /*10*/
+	"AURIGA", /*11*/
+	"INFINITE", /*12*/
+	"HEAVEN", /*13*/
+	"HIGGS BOSON", /*14*/
+	"INSPIRATION", /*15*/
+	"STANDBY", /*16*/
+	"RISE", /*17*/
+	"LUNA", /*18*/
+	"KVIGANE 2014", /*19*/
+	"EXCITE", /*20*/
+]
